ce47f09d25de03ebdc73ff428b86980889f8fc66
HEMALATHA01492 <130633006+HEMALATHA01492@users.noreply.github.com>
2023-05-04 13:57:45 +0530

    QUES1
    compare Two JSON have same properties without order

---
+++ DAY3TASKS.js
@@ -0,0 +1,31 @@
+//compare Two JSON have same properties without order
+
+
+
+let detail1=
+{            name:"HEMA",
+             gender:"female",
+             age:"25",
+             mobile:"9842390980",
+             
+}
+let detail2={ name:"HEMA",
+               mobile:"9842390980",
+               gender:"female",
+               age:"25",
+             
+          
+}
+let flag=true
+for(let e in detail1)
+{ 
+    if(detail1[e]!==detail2[e])
+{
+    flag=false;
+    break;
+    
+}
+}
+flag?console.log("Equal"):console.log("Not Equal")
+
+
